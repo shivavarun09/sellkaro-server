9df1019d16fe4ea89c74b0154a660d4f4ff50c6d
shivavarun09 <varunapps09@gmail.com>
2025-10-07 15:45:27 +0530

    this is commit

--- config/dataBaseConn.js
+++ config/dataBaseConn.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// dotenv.config();
+dotenv.config();
 const connectToDb = async()=>{
   try{
 await mongoose.connect(process.env.MONGO_URI)
-
+console.log(process.env.MONGO_URI)
 // console.log(process.env.MONGO_URI)
 console.log("Connection to database successfullllll ");
   }
