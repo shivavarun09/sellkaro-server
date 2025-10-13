ae6ea06adf41266ab23be293f6799489828553c6
shivavarun09 <varunapps09@gmail.com>
2025-10-13 14:11:54 +0530

    chnaged MOngo Uri

--- config/dataBaseConn.js
+++ config/dataBaseConn.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const connectToDb = async () => {
+  const mongoUri = process.env.MONGO_URI;
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    
+    await mongoose.connect(mongoUri);
     console.log("✅ Connected to MongoDB successfully!");
   } catch (error) {
     console.error("❌ Failed to connect to MongoDB:", error.message);
+    throw error;
   }
 };
 
